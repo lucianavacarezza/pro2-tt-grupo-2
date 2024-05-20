5f70f7b876fd9897ac782edbbd91ea93739d0cd2
melimarquezz <mmelina@udesa.edu.ar>
2024-05-20 17:49:39 -0300

    instalación de sequelize

--- myapp/controllers/indexController.js
+++ myapp/controllers/indexController.js
@@ -1,5 +1,5 @@
-const db = require ("../db/database")
-
+const db = require ("../database/models");
+// cuando hagamos el modelo usemos la referencia del ppt= const movie= db.alias delmmodelo
 const indexController = {
     index: function (req, res) {
         return res.render ("index", {data: db.productos})
