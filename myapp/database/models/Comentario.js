5b7a066e5f0f49adc3a465699c9680ef7d43e71d
luciana vacarezza <lucianavacarezza@gmail.com>
2024-06-10 16:08:15 -0300

    creacion de modelo comentario

---
+++ myapp/database/models/Comentario.js
@@ -0,0 +1,39 @@
+const { FOREIGNKEYS } = require("sequelize/lib/query-types");
+
+module.exports = function (sequelize, dataTypes) {
+    let alias = "Comentario";
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        idProducto: {
+            type: dataTypes.INTEGER
+        },
+        idUsuario: {
+            type: dataTypes.INTEGER
+        },
+        texto: {
+            type: dataTypes.STRING
+        },
+        createdAt: {
+            type: dataTypes.DATE
+        },
+        updatedAt: {
+            type: dataTypes.DATE
+        }
+    }
+
+    let config = {
+        tableName: "comentarios", //si el nombre de la tabla no coincide con el del modelo en plural
+        timestamps: true, //deshabilita las columnas para timestamps en este modelo
+        underscored: false, //si los campos tienen guiones bajos
+    }
+
+    let Comentario = sequelize.define(alias, cols, config);
+
+    return Comentario;
+    
+}
